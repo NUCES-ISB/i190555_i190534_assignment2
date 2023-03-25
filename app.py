dab46b81145c33f96249292dd4f341d9551f5fb5
Safa Zaid <safazaidmalik@gmail.com>
2023-03-25 17:46:44 +0500

    Integrated model-flask app+ Fixed bug in home.html

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+import tensorflow as tf
 
 # Create flask app
 flask_app = Flask(__name__)
 model = pickle.load(open("stock_close_model.pkl", "rb"))
 
+
 @flask_app.route("/")
 def Home():
     return render_template("index.html")
 
-@flask_app.route("/predict", methods = ["POST"])
+
+@flask_app.route("/predict", methods=["POST"])
 def predict():
     float_features = [float(x) for x in request.form.values()]
-    features = [np.array(float_features)]
+    features = tf.convert_to_tensor(np.array(float_features).reshape(1, -1))
+    print('Input shape = ', features.shape)
+
+    # prediction = model.predict([np.reshape(features, (0, 0, 3))])
     prediction = model.predict(features)
-    return render_template("index.html", prediction_text = "The predicted Close value is {}".format(prediction))
+    # prediction.shape
+    return render_template("index.html", prediction_text="The predicted Close value is {}".format(prediction))
+
 
 if __name__ == "__main__":
     flask_app.run(debug=True)
--- train_livestream.py
+++ train_livestream.py
@@ -12,7 +12,7 @@ def get_training_data():
     fname = "live_stream_training_data.csv"
     try:
         # getting the live data from the stocks of the given banks till 28/02/2023
-        data = stocks(["SILK", "UBL", "HBL", "AKBL"],
+        data = stocks(["SILK", "UBL", '''"HBL", "AKBL"'''],
                       start=datetime.date(2015, 1, 1), end=datetime.date(2023, 2, 28))
         # removing the null data
         print('type(data) = ', type(data))
@@ -27,7 +27,7 @@ def get_training_data():
 def lstm_split(data, n_steps):
     X, y = [], []
     for i in range(len(data)-n_steps+1):
-        X.append(data[i:i+n_steps])
+        X.append(data[i:i+n_steps, :-1])
         y.append(data[i+n_steps-1, -1])
     return tf.convert_to_tensor(np.array(X).astype('float32')), tf.convert_to_tensor(np.array(y).astype('float32'))
 
@@ -55,7 +55,7 @@ def train_lstm():
     # print('dataframe - \n', df)
     # print('df describe() = ', df.describe())
     df = pd.read_csv('live_stream_training_data.csv')
-    feature_X = df.iloc[:, 2:5]
+    feature_X = df.iloc[:, 2:6]
     sc = StandardScaler()
     X_ft = sc.fit_transform(feature_X.values)
     X_ft = pd.DataFrame(columns=feature_X.columns,
@@ -70,9 +70,11 @@ def train_lstm():
 
     X_train, X_test = X1[:split_idx], X1[split_idx:]
     y_train, y_test = y1[:split_idx], y1[split_idx:]
-    x_train_date, x_test_date = date_index[:split_idx], date_index[split_idx:]
+    # x_train_date, x_test_date = date_index[:split_idx], date_index[split_idx:]
 
-    print(X1.shape, X_train.shape, X_test.shape, y_test.shape)
+    print(X1.shape, X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+    # Building the LSTM model
 
     lstm = Sequential()
     lstm.add(LSTM(32, input_shape=(X_train.shape[1], X_train.shape[2]),
@@ -86,6 +88,10 @@ def train_lstm():
     history = lstm.fit(X_train, y_train, epochs=10,
                        batch_size=4, verbose=2, shuffle=False)
     print(history)
+    y_pred = lstm.predict(X_test)
+    print("Predicted Close Values:", y_pred)
+    print('X_test type and shape: ', type(X_test), X_test.shape)
+    print('y_pred type and shape: ', type(y_pred), y_pred.shape)
 
     # form pkl
     import pickle
